cf7a312f8f019e10d2a4a885fe8dcb4ee9ad9e49
Daniel <daniel@test.no>
2019-10-21 20:22:11 +0200

    Create basic-mathematical-operations.php

---
+++ 8 Kyu/basic-mathematical-operations.php
@@ -0,0 +1,27 @@
+/*
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+Examples
+
+basicOp('+', 4, 7)         // Output: 11
+basicOp('-', 15, 18)       // Output: -3
+basicOp('*', 5, 5)         // Output: 25
+basicOp('/', 49, 7)        // Output: 7
+*/
+
+function basicOp($op, $val1, $val2)
+{
+    switch ($op)
+    {
+    case "+": 
+        return $val1 + $val2;
+    case "-": 
+        return $val1 - $val2;
+    case "*": 
+        return $val1 * $val2;
+    case "/": 
+        return $val1 / $val2;
+    }
+}
