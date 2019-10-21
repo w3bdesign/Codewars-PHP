d4ec7fa52e6855e3788217be560044a9110e1344
Daniel <daniel@test.no>
2019-10-21 02:23:09 +0200

    Update remove-first-and-last-character.php

--- 8 Kyu/remove-first-and-last-character.php
+++ 8 Kyu/remove-first-and-last-character.php
@@ -1,16 +1,21 @@
-/*
-It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. 
-You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
-*/
-
-function remove_char(string $s): string {
-$removeFirst = substr($s, 1);
-$finalString = substr($removeFirst, 0, strlen($removeFirst)-1);
-return $finalString;
+<?php
+/**
+ * It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. 
+ * You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+ */
+function Remove_Char(string $s)
+{
+    $removeFirst = substr($s, 1);
+    $finalString = substr($removeFirst, 0, strlen($removeFirst)-1);
+    return $finalString;
 }
 
-// Alternate solution:
+/**
+ * Alternate solution
+ */
 
-function remove_char(string $s): string {
-  return substr($s, 1, -1);
+function Remove_Char(string $s)
+{
+    return substr($s, 1, -1);
 }
+?>
