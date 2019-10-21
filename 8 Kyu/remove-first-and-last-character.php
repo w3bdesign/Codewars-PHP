1c3a5e2a9410903d595d15d04ef174c2b2fa2869
Daniel <daniel@test.no>
2019-10-21 20:14:55 +0200

    New solution

--- 8 Kyu/remove-first-and-last-character.php
+++ 8 Kyu/remove-first-and-last-character.php
@@ -1,4 +1,3 @@
-<?php
 /**
  * It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. 
  * You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
@@ -18,4 +17,3 @@ function Remove_Char(string $s)
 {
     return substr($s, 1, -1);
 }
-?>
---
+++ 8 Kyu/reversed-sequence.php
@@ -0,0 +1,22 @@
+/*
+Get the number n (n>0) to return the reversed sequence from n to 1.
+
+Example : n=5 >> [5,4,3,2,1]
+*/
+
+function reverseSeq($n)
+{
+    $number = [];
+    for($i = $n; $i > 0;$i--)
+    {
+        array_push($number, $i);
+    }
+    return $number;  
+};
+
+// Alternate solution:
+
+function reverseSeq($n)
+{
+    return range($n, 1);
+};
