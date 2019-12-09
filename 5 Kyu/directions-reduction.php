502e13e699c8136569e0e35aaaeb2535b9371758
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2019-12-09 08:12:09 +0100

    Removing unnecessary text
    Removing unnecessary text in comment section in the beginning

--- 5 Kyu/directions-reduction.php
+++ 5 Kyu/directions-reduction.php
@@ -39,10 +39,6 @@ In ["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"], "NORTH" and "SOUTH" are n
 Task
 
 Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
-
-    The Haskell version takes a list of directions with data Direction = North | East | West | South.
-    The Clojure version returns nil when the path is reduced to nothing.
-    The Rust version takes a slice of enum Direction {NORTH, SOUTH, EAST, WEST}.
 */
 
 function dirReduc($array)
