d0fbd1a2e4411c89d6d66dacb686e7fdde7d3e74
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-02-16 17:39:30 +0100

    Fix code quality issues

--- 6 Kyu/multiples-of-3-or-5.php
+++ 6 Kyu/multiples-of-3-or-5.php
@@ -9,6 +9,7 @@ Note: If the number is a multiple of both 3 and 5, only count it once.
 
 function solution($number)
 {
+    $sum = 0;
     for ( $i = 3; $i < $number; $i++) {
 
         if ($i % 3 === 0 || $i % 5 === 0) {   
@@ -20,7 +21,7 @@ function solution($number)
 
 // Alternate solution:
 
-function solution($number)
+function altSolution($number)
 {
     return array_sum(
         array_filter(
@@ -30,4 +31,3 @@ function solution($number)
         )
     );
 }
-?>
